2e66524f5005ec12b2c56ca6934bcdfd131169e1
ktunprasert <ktunprasert@outlook.com>
2023-09-29 11:53:27 +0100

    wdg.scraper.scraper: logging, edge case and more con

--- apps/wdgscraper/lib/wdg/scraper.ex
+++ apps/wdgscraper/lib/wdg/scraper.ex
@@ -2,13 +2,16 @@ defmodule WDG.Scraper do
   alias WDG.Chan
   alias WDG.Parser
 
-  def scrape_for_wdg(post_ids, con \\ 10) do
+  def scrape_for_wdg(post_ids, con \\ 50) do
     Task.async_stream(
       post_ids,
       fn
         post_id ->
+          IO.puts("Scraping post #{post_id}")
+
           with thread <- Chan.get_thread(post_id),
                true <- thread |> Chan.is_thread_wdg?() do
+            IO.puts("Thread matched #{post_id} parsing...")
             [_op | non_op_posts] = thread |> Map.get("posts")
 
             non_op_posts
@@ -30,9 +33,13 @@ defmodule WDG.Scraper do
             end)
           end
       end,
-      max_concurrency: con
+      max_concurrency: con,
+      timeout: :infinity
     )
-    |> Enum.reduce([], fn {:ok, posts}, acc -> acc ++ posts end)
+    |> Enum.reduce([], fn
+      {:ok, false}, acc -> acc
+      {:ok, posts}, acc -> Enum.concat(acc, posts)
+    end)
   end
 
   def insert_posts(posts) do
@@ -51,10 +58,10 @@ defmodule WDG.Scraper do
           tools: tools,
           repo: repo
         },
-        "no" => post_num,
-        "ext" => ext,
-        "tim" => filename
+        "no" => post_num
       } = post ->
+        {filename, ext} = {Map.get(post, "tim"), Map.get(post, "ext")}
+
         image =
           if Map.get(post, "filename") != nil do
             {:ok, %{body: image}} = HTTPoison.get("https://i.4cdn.org/g/#{filename}#{ext}")

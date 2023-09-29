3a6d72ecca8ef8da73ce2ebe69bfa5c6128b3074
ktunprasert <ktunprasert@outlook.com>
2023-09-29 10:26:36 +0100

    wdg.scraper: added Scraper module

---
+++ apps/wdgscraper/lib/wdg/scraper.ex
@@ -0,0 +1,38 @@
+defmodule WDG.Scraper do
+  alias WDG.Chan
+  alias WDG.Parser
+
+  def scrape_for_wdg(post_ids, con \\ 10) do
+    Task.async_stream(
+      post_ids,
+      fn
+        post_id ->
+          with thread <- Chan.get_thread(post_id),
+               true <- thread |> Chan.is_thread_wdg?() do
+            [_op | non_op_posts] = thread |> Map.get("posts")
+
+            non_op_posts
+            |> Enum.map(fn post -> Map.put(post, "body", Parser.extract_post(post["com"])) end)
+            |> Enum.filter(fn post ->
+              case post["body"] do
+                nil ->
+                  false
+
+                body ->
+                  count =
+                    body
+                    |> Map.to_list()
+                    |> Enum.count(fn {_, v} -> v == nil end)
+
+                  # if the nil count is less than or equal to 5, then it's a valid post
+                  count <= 5
+              end
+            end)
+          end
+      end,
+      max_concurrency: con
+    )
+    |> Enum.reduce([], fn {:ok, posts}, acc -> acc ++ posts end)
+    |> dbg
+  end
+end
